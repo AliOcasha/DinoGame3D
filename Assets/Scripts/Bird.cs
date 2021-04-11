894b9ec8e812adb1b3d3cb9fe7b97a91028700c0
AliOcasha <aliocasha@gmail.com>
2021-04-11 16:15:26 +0200

    Corrected Orientation // implemented Animation Stop on Collision

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     private Spawner BirdSpawner;
+    private Animator Animator;
     private readonly Quaternion BaseRot = Quaternion.Euler(0f, 90f, 0f);
-    private readonly Vector3 Movement = Vector3.up * 1.5f;
+    private readonly Vector3 Movement = Vector3.forward * 1.5f;
     private void Start()
     {
         // Get Spawner Script
         BirdSpawner = GameObject.FindObjectOfType<Spawner>();
+        //Get Animator Component
+        Animator = gameObject.GetComponent<Animator>();
         // Set Rotation for Bird
         transform.rotation = BaseRot;
     }
@@ -29,5 +32,16 @@ public class Bird : MonoBehaviour
             Destroy(gameObject, 2);
         }
 
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Animator.SetBool("Flying", false);
+        }
+        else
+        {
+            Animator.SetBool("Flying", true);
+        }
     }
 }
